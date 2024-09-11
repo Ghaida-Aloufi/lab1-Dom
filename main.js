674898e363ae1975c9aa7ecb01496ae9600252ce
Ghaida-Aloufi <ghaida2219@gmail.com>
2024-09-11 12:41:30 +0300

    edit lab1 dom

--- main.js
+++ main.js
@@ -1,8 +1,16 @@
 
 //Q1
-let x = document.getElementsByClassName("text1")[0]
+let x = document.getElementById("text1")
+
 console.log(x.style.color = 'white');
 console.log(x.style.backgroundColor = 'green');
+
+//Q2
+
+let y=document.getElementById("test1")
+y.setAttribute("class","p2")
+
+
 
 
 // Q3
@@ -12,15 +20,56 @@ console.log(img.src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQEOm5
 
 //Q4
 let ul=document.querySelector("ul")
+let var1=document.createElement("li")
+var1.textContent="HTML"
+ul.appendChild(var1)
 
-console.log(ul.appendChild("li").innerText="hello".style.color="pink");
-console.log(li.style.border = '2px solid black');
+let var2=document.createElement("li")
+var2.textContent="CSS"
+ul.appendChild(var2)
 
-console.log(ul.appendChild("li").innerText="hello".style.color="pink");
-console.log(li.style.border = '2px solid black');
+let var3=document.createElement("li")
+var3.textContent="JS"
+ul.appendChild(var3)
 
-console.log(ul.appendChild("li").innerText="hello".style.color="pink");
-console.log(li.style.border = '2px solid black');
+ul.setAttribute("class","ul")
+
+
+
+
+//Q5
+
+
+let table=document.createElement("table")
+document.body.appendChild(table)
+let row1=document.createElement("tr")
+let row2=document.createElement("tr")
+let col1=document.createElement("td")
+let col2=document.createElement("td")
+let col3=document.createElement("td")
+let col4=document.createElement("td")
+let col5=document.createElement("td")
+let col6=document.createElement("td")
+col1.innerText="HTML"
+col2.innerText="JS"
+col3.innerText="CSS"
+col4.innerText="SQL"
+col5.innerText="API"
+col6.innerText="DOM"
+table.appendChild(row1)
+table.appendChild(row2)
+row1.appendChild(col1)
+row1.appendChild(col2)
+row1.appendChild(col3)
+row2.appendChild(col4)
+row2.appendChild(col5)
+row2.appendChild(col6)
+
+table.setAttribute("class","table")
+
+
+
+
 
 
 
