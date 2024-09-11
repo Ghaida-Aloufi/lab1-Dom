bd53aeeab1410c06735c54fabfcf012fb27d4695
Ghaida-Aloufi <ghaida2219@gmail.com>
2024-09-11 11:57:11 +0300

    add lab1 dom

---
+++ main.js
@@ -0,0 +1,37 @@
+
+//Q1
+let x = document.getElementsByClassName("text1")[0]
+console.log(x.style.color = 'white');
+console.log(x.style.backgroundColor = 'green');
+
+
+// Q3
+
+let img = document.getElementsByTagName("img")[0]
+console.log(img.src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQEOm5WwJq_NDFkqnaJpamwql8hFefcZPuYpA&s");
+
+//Q4
+let ul=document.querySelector("ul")
+
+console.log(ul.appendChild("li").innerText="hello".style.color="pink");
+console.log(li.style.border = '2px solid black');
+
+console.log(ul.appendChild("li").innerText="hello".style.color="pink");
+console.log(li.style.border = '2px solid black');
+
+console.log(ul.appendChild("li").innerText="hello".style.color="pink");
+console.log(li.style.border = '2px solid black');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
